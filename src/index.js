a5055f55c5ad201866da078cbe8f76acdc0800ae
unknown <pavel.build@gmail.com>
2020-10-02 11:21:48 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = array[0];
+  for(let i=1; i < array.length; i++) {
+    let currentValue = array[i];
+    if (currentValue < minValue ) {
+      minValue = currentValue;
+    }
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = array[0];
+  for(let i=1; i < array.length; i++) {
+    let currentValue = array[i];
+    if (currentValue > maxValue ) {
+      maxValue = currentValue;
+    }
+  }
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let total = 0;
+  for ( let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  let avgValue = total / array.length
+  return avgValue;
 }
